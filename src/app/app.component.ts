36281741d1b6db5f7ffd90e4e22e57277d726d89
pomb95 <paul.buguet@gmail.com>
2019-07-25 01:28:01 +0200

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,22 +8,23 @@ import { Component } from '@angular/core';
 export class AppComponent  {
 
   list: Test[] = [
-    new Test(new Date( 1995, 6, 25)),
-    new Test(new Date( 2018, 7, 23)),
-    new Test(new Date(2003, 5, 1)),
-    new Test(new Date(1980, 2, 24))
+    new Test(1, new Date( 1995, 6, 25)),
+    new Test(2, new Date( 2018, 7, 23)),
+    new Test(5, new Date(2003, 5, 1)),
+    new Test(4, new Date(1980, 2, 24))
   ];
 
   tri() : void {
     console.log(
-      this.list.map(x => x.date)
-      .sort()
+      this.list.sort((x, y) => x.date - y.date)
     );
+    console.log(this.list);
   }
   
 }
 
 export class Test {
   constructor (
+    id: number,
     public date: Date) {}
 }
