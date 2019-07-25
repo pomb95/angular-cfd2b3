d103f9caf0963b9549d83011c5d9f46884ccc402
pomb95 <paul.buguet@gmail.com>
2019-07-25 22:50:23 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,11 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent  {
 
+  private selectedDate: Date;
+
   list: Test[] = [
-    new Test(1, new Date( 1995, 6, 25)),
-    new Test(2, new Date( 2018, 7, 23)),
-    new Test(5, new Date(2003, 5, 1)),
-    new Test(4, new Date(1980, 2, 24))
+    new Test(1, new Date( 1995, 6, 25))
+   // new Test(2, new Date( 2018, 7, 23)),
+    //new Test(5, new Date(2003, 5, 1)),
+    //new Test(4, new Date(1980, 2, 24))
   ];
 
   tri() : void {
@@ -19,6 +21,14 @@ export class AppComponent  {
       this.list.sort((x, y) => x.date - y.date)
     );
     console.log(this.list);
+    console.log(this.selectedDate);
+  }
+
+  addDate(): void {
+   this.list.push(
+     new Test(0, this.selectedDate)
+   );
+   console.log('date ajoutée : ', this.selectedDate);
   }
   
 }
